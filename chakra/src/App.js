6d12eaaf4d46f4ec3c5414d14f31ef9defd18ab9
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-01-21 12:59:58 +0530

    grid component

--- chakra/src/App.js
+++ chakra/src/App.js
@@ -1,23 +1,54 @@
-import { Container, Flex, Heading, Text, VStack } from "@chakra-ui/react";
-
+import {
+  Container,
+  Flex,
+  FormControl,
+  FormLabel,
+  GridItem,
+  Heading,
+  Input,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <Container maxWidth="container.lg">
-     <Flex h="100vh" py={20}>
-      
-      <VStack bg={"green.400"} w="full" p={10} spacing = {10} align = "flex-start" >
-      <VStack align={"flex-start" } spacing = {2} >
-
-      <Heading>Your details</Heading>
-      <Text>hello, this is  Chakra UI  tutorial</Text>
-      </VStack>
-    
-      </VStack>
-      <VStack bg={"gray.500"} w="full" p={10} spacing = {10} align = "flex-start" >
-      </VStack>
-      
-     </Flex>
+      <Flex h="100vh" py={20}>
+        <VStack
+          bg={"green.400"}
+          w="full"
+          p={10}
+          spacing={10}
+          align="flex-start"
+        >
+          <VStack align={"flex-start"} spacing={2}>
+            <Heading>Your details</Heading>
+            <Text>hello, this is Chakra UI tutorial</Text>
+          </VStack>
+          <SimpleGrid columns={2} columnGap={3} rowGap={2}>
+            <GridItem colSpan={1}>
+              <FormControl>
+                <FormLabel>First Name</FormLabel>
+                <Input placeholder="Enter Your First Name" />
+              </FormControl>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <FormControl>
+                <FormLabel>last Name</FormLabel>
+                <Input placeholder="Enter Your last Name" />
+              </FormControl>
+            </GridItem>
+          </SimpleGrid>
+        </VStack>
+        <VStack
+          bg={"gray.500"}
+          w="full"
+          p={10}
+          spacing={10}
+          align="flex-start"
+        ></VStack>
+      </Flex>
     </Container>
   );
 }
