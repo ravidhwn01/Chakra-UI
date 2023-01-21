0ed77c3e4945be958d5d0de52a56689d8b3f94b0
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-01-21 12:49:49 +0530

    Vstack component

--- chakra/src/App.js
+++ chakra/src/App.js
@@ -1,14 +1,22 @@
-import { Container, Flex, VStack } from "@chakra-ui/react";
+import { Container, Flex, Heading, Text, VStack } from "@chakra-ui/react";
 
 
 function App() {
   return (
     <Container maxWidth="container.lg">
      <Flex h="100vh" py={20}>
+      
       <VStack bg={"green.400"} w="full" p={10} spacing = {10} align = "flex-start" >
+      <VStack align={"flex-start" } spacing = {2} >
+
+      <Heading>Your details</Heading>
+      <Text>hello, this is  Chakra UI  tutorial</Text>
+      </VStack>
+    
       </VStack>
       <VStack bg={"gray.500"} w="full" p={10} spacing = {10} align = "flex-start" >
       </VStack>
+      
      </Flex>
     </Container>
   );
