eebefcb6b85884bd16c2fb0c178490f85496b154
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-01-21 12:23:34 +0530

    creating various compenents

--- chakra/src/App.js
+++ chakra/src/App.js
@@ -1,10 +1,16 @@
+import { Container, Flex, VStack } from "@chakra-ui/react";
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello, this is  Chakra UI  tutorial</h1>
-    </div>
+    <Container maxWidth="container.lg">
+     <Flex h="100vh" py={20}>
+      <VStack bg={"green.400"} w="full" p={10} spacing = {10} align = "flex-start" >
+      </VStack>
+      <VStack bg={"gray.500"} w="full" p={10} spacing = {10} align = "flex-start" >
+      </VStack>
+     </Flex>
+    </Container>
   );
 }
 
